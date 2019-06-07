df864dada49e08133f9f1f34cdb7214c0c4f6290
Sam Smith <sam@mintcanary.com>
2019-06-07 09:25:30 +0100

    search page

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const { spacing } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        gutter: spacing[6]
+      }
+    },
+    colors: {
+      primary: '#4E4E4F',
+      secondary: '#EF4123',
+      white: '#fff',
+      'white-75': 'rgba(255,255,255,0.75)',
+      gray: {
+        '100': '#F1F2F2',
+        '500': '#6D6E71',
+        '900': '#4E4E4F',
+      }
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+    }
+  }
+}
