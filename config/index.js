e8e8a8888329ad7a2c5946f38ce3f6383f5222af
anuveyatsu <anuar.ustayev@gmail.com>
2019-08-05 17:55:04 +0600

    [setup][s]: fix leftovers, remove extra dir, fix passing config var

--- config/index.js
+++ config/index.js
@@ -27,9 +27,7 @@ nconf.defaults({
   PLUGIN_DIR: process.env.PLUGIN_DIR || 'plugins',
   // CKAN pages PLUGIN
   CKAN_PAGES_URL: process.env.CKAN_PAGES_URL || api_url,
-  CKAN_PAGES_URL: process.env.CKAN_PAGES_URL || api_url,
-  // dashboard-js PLUGIN
-  GITHUB_BASEURL: process.env.GITHUB_BASEURL || 'https://raw.githubusercontent.com',
+  // dashboard PLUGIN
   GIT_BASE_URL: process.env.GIT_BASE_URL || 'https://raw.githubusercontent.com'
 })
 
