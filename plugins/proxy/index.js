3e5a36de784ad993c07bf95df9064b88a2be9938
anuveyatsu <anuar.ustayev@gmail.com>
2019-09-06 16:25:10 +0600

    [proxy][xs]: make it optional

--- plugins/proxy/index.js
+++ plugins/proxy/index.js
@@ -5,15 +5,19 @@ module.exports = function(app) {
   const datastore = config.get('PROXY_DATASTORE')
   const filestore = config.get('PROXY_FILESTORE')
 
-  app.use(`/proxy/datastore`, proxy(datastore, {
-    filter: function(req, res) {
-     return req.method == 'GET'
-    }
-  }))
+  if (datastore) {
+    app.use(`/proxy/datastore`, proxy(datastore, {
+      filter: function(req, res) {
+       return req.method == 'GET'
+      }
+    }))
+  }
 
-  app.use(`/proxy/filestore`, proxy(filestore, {
-    filter: function(req, res) {
-     return req.method == 'GET'
-    }
-  }))
+  if (filestore) {
+    app.use(`/proxy/filestore`, proxy(filestore, {
+      filter: function(req, res) {
+       return req.method == 'GET'
+      }
+    }))
+  }
 }
