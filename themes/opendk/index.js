b11c9c246354dafa0f3d70f7481a26bb953abc5c
anuveyatsu <anuar.ustayev@gmail.com>
2019-09-20 11:40:25 +0600

    [featured collections][s]: display featured collections randomly.
    * Get collections and filter featured items. We define featured collections via 'extras' property: {"key": "featured", "value": true}.
    * If featured collections length is not less than 4 then shuffle it and display random 4.
    * If we don't have at least 4 featured collections then show random 4 from entire list.

--- themes/opendk/index.js
+++ themes/opendk/index.js
@@ -4,8 +4,26 @@ module.exports = function (app) {
   const Model = new dms.DmsModel(config)
 
   app.get('/', async (req, res,next) => {
-      const collections = await Model.getCollections()
-      res.locals.collections = collections
-      next()
+    // Get collections with extras
+    const collections = await Model.getCollections({
+      all_fields: true,
+      include_extras: true,
+      include_dataset_count: false
+    })
+    // Filter collections as we want to show only featured items
+    const featured = collections.filter(collection => {
+      return collection.extras.find(extra => extra.key === 'featured' && extra.value)
+    })
+    // Shuffle array
+    let shuffled
+    if (featured.length >= 4) {
+      shuffled = featured.sort(() => 0.5 - Math.random())
+    } else {
+      shuffled = collections.sort(() => 0.5 - Math.random())
+    }
+    // Get sub-array of first n elements after shuffled
+    const randomFour = shuffled.slice(0, 4)
+    res.locals.collections = randomFour
+    next()
   })
 }
