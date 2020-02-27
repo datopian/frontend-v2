4827313b1aa9fe3190315fedc7236cde1f0f5c51
Paul Walker <starsinmypockets@gmail.com>
2020-02-27 11:44:48 -0500

    [checkpoint] login via API nominally works here

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 "use strict"
 const express = require("express")
+const { resolve, URL } = require('url')
 const config = require("../config")
+const fetch = require('node-fetch')
 
 module.exports = function(app) {
   const router = express.Router()
@@ -25,26 +27,57 @@ module.exports = function(app) {
     res.redirect("/")
   })
 
-  app.post("/login-form-submit", (req, res) => {
+  app.get('foobar', (req, res) => {
+    res.send('Hello foobar')
+  })
+
+  app.post("/login-form-submit", async (req, res) => {
     console.log("Login submit", req.body)
-    // TODO validate form
-    // TODO get user via api
-    req.session.ckan_user = {
-      email_hash: "2ab10b220889a2ddc3a2ffe27688b83c",
-      about: null,
-      apikey: "REDACTED",
-      display_name: "REDACTED",
-      name: "REDACTED",
-      created: "2019-11-20T10:54:23.186088",
-      id: "886811d2-15e6-4460-b1c6-86d6f78352bd",
-      sysadmin: true,
-      activity_streams_email_notifications: false,
-      state: "active",
-      number_of_edits: 0,
-      fullname: null,
-      email: "REDACTED",
-      number_created_packages: 0
+    
+    // TODO validate form here
+    
+    // TODO endpoints should be generic, or configured
+    const action = "national_grid_user_login"
+    const url = new URL(resolve(config.get('API_URL'), action))
+    const params = {
+        id: req.body.username,
+        password: req.body.password
+      }
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': config.get('API_KEY')
+        },
+        body: JSON.stringify(params)
+      })
+
+      const body = await response.json()
+      console.log(body)
+      req.session.ckan_user = body.result 
+    } catch (e) {
+      console.error('Error getting logged in user', e)
     }
+
+    /* req.session.ckan_user = { */
+    /*   email_hash: "2ab10b220889a2ddc3a2ffe27688b83c", */
+    /*   about: null, */
+    /*   apikey: "REDACTED", */
+    /*   display_name: "REDACTED", */
+    /*   name: "REDACTED", */
+    /*   created: "2019-11-20T10:54:23.186088", */
+    /*   id: "886811d2-15e6-4460-b1c6-86d6f78352bd", */
+    /*   sysadmin: true, */
+    /*   activity_streams_email_notifications: false, */
+    /*   state: "active", */
+    /*   number_of_edits: 0, */
+    /*   fullname: null, */
+    /*   email: "REDACTED", */
+    /*   number_created_packages: 0 */
+    /* } */
+
     res.redirect("/")
   })
 
@@ -52,6 +85,7 @@ module.exports = function(app) {
     // TODO - validate form
     // TODO -- call user_create handle res
     console.log(req.body)
+
     res.redirect("/")
   })
 
