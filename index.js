0e4bee2cb59c7e943546cdcad457b1e0f9acc973
anuveyatsu <anuar.ustayev@gmail.com>
2020-04-24 16:06:43 +0600

    Use 'express-async-errors' for handling exceptions and remove all try catch blocks from the routes.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+require('express-async-errors')
 const cors = require('cors')
 const nunjucks = require('nunjucks')
 const bodyParser = require('body-parser')
