831a1d41d8d3c7b092c836f27e26bec61241c2a5
anuveyatsu <anuar.ustayev@gmail.com>
2021-03-23 12:22:54 +0600

    [showcase][xs]: pass schema instead of dp id for new data explorer.

--- routes/dms.js
+++ routes/dms.js
@@ -148,7 +148,7 @@ module.exports = function () {
         avatar: profile.image_display_url || profile.image_url
       },
       thisPageFullUrl: '//' + req.get('host') + req.originalUrl,
-      dpId: JSON.stringify(datapackage).replace(/'/g, "&#x27;") // keep for backwards compat?
+      schema: JSON.stringify(datapackage.resources[0].schema).replace(/'/g, "&#x27;") // keep for backwards compat?
     })
   })
 
