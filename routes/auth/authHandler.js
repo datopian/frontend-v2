e717dedb91ea3e12c35846b46773d576841abdbd
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2022-10-07 11:12:28 -0300

    feat: phone editing routes

--- routes/auth/authHandler.js
+++ routes/auth/authHandler.js
@@ -1,53 +1,52 @@
-const config = require('../../config')
-const logger = require('../../utils/logger')
-const { Configuration, PublicApi } = require('@oryd/kratos-client')
-
+const config = require("../../config");
+const logger = require("../../utils/logger");
+const { Configuration, PublicApi } = require("@oryd/kratos-client");
 
 // A simple express handler that shows the login / registration screen.
 // Argument "type" can either be "login" or "registration" and will
 // fetch the form data from ORY Kratos's Public API.
-const kratos = new PublicApi(new Configuration({ basePath: config.get('kratos').public }))
+const kratos = new PublicApi(
+  new Configuration({ basePath: config.get("kratos").public })
+);
 
-module.exports.authHandler = (type) => (
-  req,
-  res,
-  next
-) => {
-  const flow = req.query.flow
+module.exports.authHandler = (type) => (req, res, next) => {
+  const flow = req.query.flow;
 
   // The request is used to identify the login and registration request and
   // return data like the csrf_token and so on.
   if (!flow) {
-    logger.warn('No flow ID found in URL, initializing login flow.')
+    logger.warn("No flow ID found in URL, initializing login flow.");
     res.redirect(`/.ory/kratos/public/self-service/${type}/browser`);
-    return
+    return;
   }
 
   const authRequest =
-    type === 'login'
+    type === "login"
       ? kratos.getSelfServiceLoginFlow(flow)
-      : kratos.getSelfServiceRegistrationFlow(flow)
+      : kratos.getSelfServiceRegistrationFlow(flow);
 
   authRequest
-    .then(({ status, data: flow}) => {
+    .then(({ status, data: flow }) => {
       if (status == 404 || status == 410 || status == 403) {
-        res.redirect(
-          `/.ory/kratos/public/self-service/${type}/browser`
-        )
-        return
+        res.redirect(`/.ory/kratos/public/self-service/${type}/browser`);
+        return;
       } else if (status != 200) {
-        return Promise.reject(flow)
+        return Promise.reject(flow);
       }
-      
-      flow.methods.oidc.config.action = new URL(flow.methods.oidc.config.action).pathname
 
-      res.locals.sso = flow.methods.oidc.config
-      res.locals.password = flow.methods.password.config
-      res.locals.accountExists = res.locals.sso.fields.find(item => item.name === 'traits.email')
-      next()
+      flow.methods.oidc.config.action = new URL(
+        flow.methods.oidc.config.action
+      ).pathname;
+
+      res.locals.sso = flow.methods.oidc.config;
+      res.locals.password = flow.methods.password.config;
+      res.locals.accountExists = res.locals.sso.fields.find(
+        (item) => item.name === "traits.email"
+      );
+      next();
     })
-    .catch(err => {
-      logger.error(err)
-      next(err)
-    })
-}
+    .catch((err) => {
+      logger.error(err);
+      next(err);
+    });
+};
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,87 +1,110 @@
-const request = require('request')
-const {  Configuration, PublicApi, AdminApi } = require('@oryd/kratos-client')
-const config = require('../../config')
-const { authHandler } = require('./authHandler')
-const { dashboard } = require('./dashboard')
-const { errorHandler } = require('./errorHandler')
-const logger = require('../../utils/logger')
-const proxy = require('express-http-proxy')
+const request = require("request");
+const { Configuration, PublicApi, AdminApi } = require("@oryd/kratos-client");
+const config = require("../../config");
+const { authHandler } = require("./authHandler");
+const { dashboard } = require("./dashboard");
+const { errorHandler } = require("./errorHandler");
+const logger = require("../../utils/logger");
+const proxy = require("express-http-proxy");
+const bodyParser = require("body-parser");
+const { admin } = require("googleapis/build/src/apis/admin");
 
-const kratos = new PublicApi(new Configuration({ basePath: config.get('kratos').public }))
-const adminApi = new AdminApi(new Configuration({ basePath: config.get('kratos').admin }))
+const kratos = new PublicApi(
+  new Configuration({ basePath: config.get("kratos").public })
+);
+const adminApi = new AdminApi(
+  new Configuration({ basePath: config.get("kratos").admin })
+);
 
 const protect = (req, res, next) => {
   // When using ORY Oathkeeper, the redirection is done by ORY Oathkeeper.
   // Since we're checking for the session ourselves here, we redirect here
   // if the session is invalid.
-  kratos.whoami(
-    req.header('Cookie'),
-    req.header('Authorization'),
-  ).then(({ data: session }) => {
-    // `whoami` returns the session or an error. We're changing the type here
-    // because express-session is not detected by TypeScript automatically.
-    req.user = { session }
-    next()
-  }).catch(() => {
-    // If no session is found, redirect to login.
-    res.redirect('/auth/login')
-  })
-}
+  kratos
+    .whoami(req.header("Cookie"), req.header("Authorization"))
+    .then(({ data: session }) => {
+      // `whoami` returns the session or an error. We're changing the type here
+      // because express-session is not detected by TypeScript automatically.
+      req.user = { session };
+      next();
+    })
+    .catch(() => {
+      // If no session is found, redirect to login.
+      res.redirect("/auth/login");
+    });
+};
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use((req, res, next) => {
     if (req.cookies.ory_kratos_session) {
       kratos
-        .whoami(
-          req.header('Cookie'),
-          req.header('Authorization'),
-        )
+        .whoami(req.header("Cookie"), req.header("Authorization"))
         .then(({ status, data: flow }) => {
           res.locals.logged_in = true;
           res.locals.userEmail = flow.identity.traits.email;
           res.locals.userId = flow.identity.id;
           res.locals.userName = flow.identity.traits.name;
-          next()
+          res.locals.userPhone = flow.identity.traits.phone;
+          next();
         })
         .catch(() => {
-          next()
-        })
+          next();
+        });
     } else {
-      next()
+      next();
     }
-  })
+  });
 
-  app.use('/.ory/kratos/public/', (req, res, next) => {
+  app.use("/.ory/kratos/public/", (req, res, next) => {
     const url =
-      config.get('kratos').public + req.url.replace('/.ory/kratos/public', '')
+      config.get("kratos").public + req.url.replace("/.ory/kratos/public", "");
     req
-      .pipe(
-        request(url, { followRedirect: false })
-          .on('error', err => next)
-      )
-      .pipe(res)
-  })
+      .pipe(request(url, { followRedirect: false }).on("error", (err) => next))
+      .pipe(res);
+  });
 
-  app.get('/dashboard', protect, dashboard)
-  app.get('/auth/registration', authHandler('registration'))
-  app.get('/auth/login', authHandler('login'))
-  app.get('/auth/logout', (req, res) => {
-    res.redirect('/.ory/kratos/public/self-service/browser/flows/logout')
-  })
-  app.post('/auth/delete', protect, (req, res, next) => {
-    adminApi.deleteIdentity(res.locals.userId)
-      .then(response => {
-        res.redirect('/auth/registration')
+  app.get("/dashboard", protect, dashboard);
+  app.get("/auth/registration", authHandler("registration"));
+  app.get("/auth/login", authHandler("login"));
+  app.get("/auth/logout", (req, res) => {
+    res.redirect("/.ory/kratos/public/self-service/browser/flows/logout");
+  });
+  app.post("/auth/delete", protect, (req, res, next) => {
+    adminApi
+      .deleteIdentity(res.locals.userId)
+      .then((response) => {
+        res.redirect("/auth/registration");
       })
-      .catch(err => {
-        logger.error(err)
+      .catch((err) => {
+        logger.error(err);
         req.flash(
-          'info',
-          'We could not delete your account this time. Please, try again later. If the issue persists, please contact the site administration.'
-        )
-        res.redirect('/settings')
+          "info",
+          "We could not delete your account this time. Please, try again later. If the issue persists, please contact the site administration."
+        );
+        res.redirect("/settings");
+      });
+  });
+  app.get("/error", errorHandler);
+  app.get("/settings", protect);
+  app.use(
+    bodyParser.json({
+      extended: true,
+    })
+  );
+  app.post("/auth/edit", protect, (req, res, next) => {
+    adminApi
+      .updateIdentity(res.locals.userId, {
+        traits: {
+          phone: req.body.phone,
+          email: res.locals.userEmail,
+          name: res.locals.userName,
+        },
       })
-  })
-  app.get('/error', errorHandler)
-  app.get('/settings', protect)
-}
+      .then(() => {
+        res.send({ status: 200 });
+      })
+      .catch((err) => {
+        res.send({ status: 500 });
+      });
+  });
+};
